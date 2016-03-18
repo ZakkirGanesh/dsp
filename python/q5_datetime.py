7c9c812dae2ca071fc5df2aa926c8bd46de0cd34
ZakkirGanesh <sg3142@nyu.edu>
2016-03-17 22:36:02 -0400

    Finished code for Q5

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -10,4 +10,15 @@ date_stop = '05282015'
 
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+
+from datetime import date
+
+date_diff1 = date(2015, 7, 28) - date(2013, 1, 2)
+print(date_diff1)
+
+date_diff2 = date(2015, 5, 28) - date(2013, 12, 31)
+print(date_diff2)
+
+date_diff3 = date(2015, 7, 14) - date(1994, 1, 15)
+print(date_diff3)
