1416a7949d8e4c550035f509bca7dc83d7e708db
ZakkirGanesh <sg3142@nyu.edu>
2016-03-18 19:53:28 -0400

    Updated code for Q8

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -17,3 +17,24 @@ import csv
 
   def get_team(self, index_value, parsed_data):
     # COMPLETE THIS FUNCTION
+
+import csv
+
+football_file = list(csv.reader(open("/Users/azg/ds/metis/dsp/python/football.csv")))
+
+goals_list = [int(team[5]) for team in football_file[1:]]
+goals_allowed_list = [int(team[6]) for team in football_file[1:]]
+team_names_list = [team[0] for team in football_file[1:]]
+goal_diff = []
+for i in range(len(football_file[1:])):
+    goal_diff.append(abs(goals_list[i] - goals_allowed_list[i]))
+    i += 1
+
+smallest_diff = None
+sd_index = None
+for diff in goal_diff:
+    if smallest_diff is None or diff < smallest_diff:
+        smallest_diff = diff
+        sd_index = goal_diff.index(diff)
+print(smallest_diff)
+print(team_names_list)
