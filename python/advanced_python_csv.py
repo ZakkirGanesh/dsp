260ae5242d65cb176355c314178f4e72dc183755
ZakkirGanesh <sg3142@nyu.edu>
2016-03-18 23:07:09 -0400

    Updated csv code

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,17 @@
-PLACE YOUR CODE HERE
+import csv
+
+faculty_list = list(csv.reader(open
+            ("/Users/azg/ds/metis/dsp/python/faculty.csv")))
+
+email_csv_list = csv.writer(open("/Users/azg/ds/metis/dsp/python/emails.csv", 
+                                 "w"), delimiter=",")
+
+email_list = [name[3] for name in faculty_list[1:]]                                 
+
+nested_email_list = []
+for email in email_list:
+    nested_email_list.append([email])
+    
+print(nested_email_list)
+for email in nested_email_list:
+    email_csv_list.writerows(email)
