a9cba3718018c3a0d4f8783f8469bc87ec3dd3c0
ZakkirGanesh <sg3142@nyu.edu>
2016-03-19 01:52:55 -0400

    Final code for dict

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,4 +1,5 @@
 import csv
+import collections
 
 faculty_list = list(csv.reader(open
             ("/Users/azg/ds/metis/dsp/python/faculty.csv")))
@@ -13,17 +14,16 @@ for name in faculty_list[1:]:
     last_names_list.append(last_name)
 
 both_names_list = []
+last_first_list = []
 for name in faculty_list[1:]:
     each_name = name[0].split(" ")
     name_tuple = [each_name[0], each_name[-1]]
     both_names_list += [name_tuple]
 both_names_tuple = tuple(tuple(entry) for entry in both_names_list)
-#print(both_names_tuple)
 
 first_faculty_dict = {}
 info_only_list = [info[1:] for info in faculty_list[1:]]
-#print(info_only_list)
-#print(faculty_list)
+
 i = 0
 while i < len(info_only_list):
     if last_names_list[i] in first_faculty_dict:
@@ -32,11 +32,24 @@ while i < len(info_only_list):
         first_faculty_dict[last_names_list[i]] = []
         first_faculty_dict[last_names_list[i]] += [info_only_list[i]]
     i += 1
-#print(first_faculty_dict)
 
 second_faculty_dict = {}
 
 for j in range(len(info_only_list)):
     second_faculty_dict[both_names_tuple[j]] = info_only_list[j]
     j += 1
-print(second_faculty_dict)
+    
+for name in faculty_list[1:]:
+    each_name = name[0].split(" ")
+    last_name_first = [each_name[-1], each_name[0]]
+    last_first_list += [last_name_first]
+last_first_tuple = tuple(tuple(entry) for entry in last_first_list)
+
+third_faculty_dict = {}
+
+for k in range(len(info_only_list)):
+    third_faculty_dict[last_first_tuple[k]] = info_only_list[k]
+    k += 1
+
+ordered_faculty_dict = collections.OrderedDict(sorted(third_faculty_dict.items()))
+
